9721612bb75b5a521c8ede4d571d27f80e5055d0
Robert Magnutzki <RobertMagnutzki@gmail.com>
2022-02-16 16:10:15 +0100

    a new version

--- main.c
+++ main.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int add(int a, int b){
+    return(a+b);
+}
 int main(){
 /* For some reason there is no code yet. It'll come soon.  */
-
-
-
-
-
-
-
-
-
+    int a;
+    int b;
+    a=add(1,2);
+    printf("%i", a);
     return 0;
 }
